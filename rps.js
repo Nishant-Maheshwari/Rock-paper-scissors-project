171de1d54b6c3d13b51bbe99fd446696a0382230
Nishant Maheshwari <nishant2622@gmail.com>
2025-05-05 00:18:09 +0530

    add score table

--- rps.js
+++ rps.js
@@ -1,3 +1,9 @@
+let score = JSON.parse(localStorage.getItem('score'))||{
+  wins:0,
+  loss:0,
+  tie:0
+}
+
 function getcomputerMovE(){
   let random =  Math.floor((Math.random()*3)+1) 
   let computerMovE = ''
@@ -12,22 +18,49 @@ function getcomputerMovE(){
   }
   return computerMovE
 } 
-function PlayerMove(MOvePicked){
+function PlayerMove(MovePicked){
   let computerMovE = getcomputerMovE()
   let result = ''
   if(
-    MOvePicked === computerMovE
+    MovePicked === computerMovE
   ){
    result = "Tie"
   }else if(
-    (MOvePicked === "rock" && computerMovE === 'scissors')||
-    (MOvePicked === "paper" && computerMovE === 'rock')||
-    (MOvePicked === "rock" && computerMovE === 'scissors')
+    (MovePicked === "rock" && computerMovE === 'scissors')||
+    (MovePicked === "paper" && computerMovE === 'rock')||
+    (MovePicked === "rock" && computerMovE === 'scissors')
   ){
- result = "you Win"
+ result = "You Win"
   }else{
-    result = "you Lose"
+    result = "You Lose"
   }
-  console.log(computerMovE);
+  scoreTable(result)
   
+  let html = document.querySelector(`.js-result`);
+html.innerHTML = `<p>computer move: ${computerMovE}-Your move: ${MovePicked}
+ ${result}
+ wins:${score.wins} loss:${score.loss} tie:${score.tie}</P>`
+setTimeout(()=>{html.innerHTML = ""},2000)
+  
+} 
+function scoreTable(result){
+  if(
+    result === "You Win"){
+      score.wins++
+    }else if(
+      result === "You Lose"
+    ){
+      score.loss++
+    }else{
+      score.tie++
+    }
+    localStrg()
+    console.log(score);
+    
+}
+
+
+
+function localStrg(){
+  localStorage.setItem('score',JSON.stringify(score))
 }
